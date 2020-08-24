bfbf0f1154ebca96ae53cfb5c029e8132d5f2472
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-08-25 02:17:22 +1200

    interval is 2 seconds

--- main/main.c
+++ main/main.c
@@ -46,9 +46,8 @@ void ble_app_advertise(void)
 
     fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_DISC_LTD;
     fields.tx_pwr_lvl_is_present = 1;
-    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
-    fields.num_uuids16 = 18;
-    fields.uuids16_is_complete = 1;
+    fields.tx_pwr_lvl = -3;
+
     fields.name = (uint8_t *)ble_svc_gap_device_name();
     fields.name_len = strlen(ble_svc_gap_device_name());
     fields.name_is_complete = 1;
@@ -59,7 +58,8 @@ void ble_app_advertise(void)
     memset(&adv_params, 0, sizeof(adv_params));
     adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
     adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
-
+    adv_params.itvl_max = 2000;
+    adv_params.itvl_min = 2000;
     ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
 }
 
